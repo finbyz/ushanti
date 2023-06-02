6e8418e9b937d4e859d52c8e9d74efc11766209d
finbyz <info@finbyz.com>
2023-06-02 07:32:53 +0000

    bank entry chage DB

--- ushanti/ushanti/doc_event/bank_statement_transaction_entry.py
+++ ushanti/ushanti/doc_event/bank_statement_transaction_entry.py
@@ -3,10 +3,11 @@ import frappe
 
 def validate(self,method):
     validate_total_of_allocated_amount(self)
+
 def on_submit(self,method):
     if self.total_amount and self.total_allocated_amount:
-   	    if flt(self.total_allocated_amount) != flt(self.total_amount):
-            frappe.throw("Total Amount of New Transaction and Total Allocated Amount are Match '<br>' Total Amount = {} '<br>' Total Allocated Amount = {}".format(frappe.bold(self.total_amount)),frappe.bold(self.total_allocated_amount))
+           if flt(self.total_allocated_amount) != flt(self.total_amount):
+            frappe.throw("Total Amount of New Transaction and Total Allocated Amount are Match '<br>' Total Amount = {0} '<br>' Total Allocated Amount = {1}".format(frappe.bold(self.total_amount),frappe.bold(self.total_allocated_amount)))
 
 def validate_total_of_allocated_amount(self):
     total_allocated_amount = 0
