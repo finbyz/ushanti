2e8c0388fd1ddfd7dbb8ba2889f5418ed400e203
Ubuntu <ubuntu@ip-172-31-28-176.ap-south-1.compute.internal>
2023-06-26 12:32:17 +0000

    bank entry DB

--- ushanti/ushanti/doc_event/bank_statement_transaction_entry.py
+++ ushanti/ushanti/doc_event/bank_statement_transaction_entry.py
@@ -4,10 +4,12 @@ import frappe
 def validate(self,method):
     validate_total_of_allocated_amount(self)
 
+# remove validation As per disscus with sagar bhai
 def on_submit(self,method):
-    if self.total_amount and self.total_allocated_amount:
-           if flt(self.total_allocated_amount) != flt(self.total_amount):
-            frappe.throw("Total Amount of New Transaction and Total Allocated Amount are Match '<br>' Total Amount = {0} '<br>' Total Allocated Amount = {1}".format(frappe.bold(self.total_amount),frappe.bold(self.total_allocated_amount)))
+    pass
+#     if self.total_amount and self.total_allocated_amount:
+#            if flt(self.total_allocated_amount) != flt(self.total_amount):
+#             frappe.throw("Total Amount of New Transaction and Total Allocated Amount are Match '<br>' Total Amount = {0} '<br>' Total Allocated Amount = {1}".format(frappe.bold(self.total_amount),frappe.bold(self.total_allocated_amount)))
 
 def validate_total_of_allocated_amount(self):
     total_allocated_amount = 0
